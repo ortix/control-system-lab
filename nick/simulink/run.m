e550dfb2006046013fa02ea9b748fe04c0cb7ee0
Ortix92 <nicktsutsunava@gmail.com>
2017-03-01 17:34:55 +0100

    PID controller with torque bypass

--- nick/simulink/run.m
+++ nick/simulink/run.m
@@ -4,8 +4,15 @@ model = 'visualize';
 load_system(model);
 
 %% Define extra parameters for model
-par.initial_state = [0 0]; % Pendulum is vertical (up up)
+par.initial_state = [pi+0.1 0]; % Pendulum is vertical (up up)
 par.torque = 0; % Input torque
+par.sim_time = 5;
+par.torque_bypass = 1; % Set to 0 to filter torque
+
+% Controller parameters
+par.Kp = -1;
+par.Kd = 0;
+par.Ki = 0;
 %% Initialize parameters and states
 hws = get_param(bdroot, 'modelworkspace');
 
