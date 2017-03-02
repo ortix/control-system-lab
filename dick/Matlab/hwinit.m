d67b14bc46ce0093ff431e211273b134c4ae44e2
dnieuwenhuizen <dick_n93@hotmail.com>
2017-03-02 16:44:25 +0100

    Alles in mijn eigen branch

---
+++ dick/Matlab/hwinit.m
@@ -0,0 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DSCS FPGA interface board: init and I/O conversions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Ts = 0.001;
+
+% gains and offsets
+daoutoffs = [0.00];                   % output offset
+daoutgain = 1*[-6];                   % output gain
+
+adinoffs = -[1.1792 1.338-0.125];
+adingain = [pi/2.6063 pi/2.3193];
+
+
+adinoffs = [adinoffs 0 0 0 0 0];    % input offset
+adingain = [adingain 1 1 1 1 1];     % input gain (to radians)
+
