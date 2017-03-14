bfaf9c1e67c4c9707ddb4239563a58986b73e070
dnieuwenhuizen <dick_n93@hotmail.com>
2017-03-14 10:23:28 +0100

    Commit voor de parameter estimation

---
+++ dick/Matlab/Non linear identification/Multisine_generation.m
@@ -0,0 +1,48 @@
+%% Choice between a multisine and a crested multisine
+
+% Creating a multisine signal as used in SIPE
+% Specifing parameters
+A = 0.3;        % Amplification of the input signal
+T_end = 10;     % end time of simulation
+fs = 100;       % sampling frequency
+fcut = 40;      % cutoff frequency for multisine signal
+dt = 1/fs;
+N = T_end/dt;
+Nseg = N/10;
+t = (0:N-1).'*dt; % time vector
+
+% Random phase multisine signal from 0 to fcut 
+fv = (0:Nseg-1)'/Nseg*fs;                % Make a new frequency for the shorter Multisine FFT 
+P  = 10;                                 % Amount of periods (10 of 20 seconds)
+Tm = T_end./P;                           % Duration of 1 period
+freqv=1/Tm:1/Tm:fcut;                    % array of frequencies
+cx=exp(1j*rand(length(freqv),1)*2*pi);   % amplitude and phase in freq domain
+[xp,~]=msinprep(freqv,cx,Nseg,fs,'screen'); % Generate time series (multisine)
+
+xp_max = max(xp);
+xp = (xp/xp_max)*A;                              % Amplitude of 1.5
+
+% Implement repmat
+xp_long = repmat(xp,P,1);                        % Create a long signal consisting of multiple periods
+
+Xp=fft(xp);                              % Fourier transform multisine
+SXp = 1/N*Xp.*conj(Xp);                  % Auto spectral density
+
+% Crested multisine signal, improved SNR!
+[cx2,crinfo]=msinclip(freqv,cx,[],[],50,[],N);  % Crested with 50 iterations
+[xpc2,~]=msinprep(freqv,cx2,Nseg,fs,'screen');
+
+xpc2_max = max(xpc2);
+xpc2 = (xpc2/xpc2_max)*A; 
+
+% Implement repmat
+xpc2_long = repmat(xpc2,P,1);                        % Create a long signal consisting of multiple periods
+
+Xpc2 = fft(xpc2);                        % Fourier transfrom crested multisine
+SXpc2 = 1/N*Xpc2.*conj(Xpc2);            % Auto spectral density
+
+figure(1);
+plot(t,xp_long,t,xpc2_long)
+figure(2);
+semilogy(fv,abs(SXp)); hold on 
+semilogy(fv,abs(SXpc2));
--- dick/Matlab/Non linear identification/nonlinid_pendulum.m
+++ dick/Matlab/Non linear identification/nonlinid_pendulum.m
@@ -1,6 +1,45 @@
-% estimate (fine-tune) a parameter in a nonlinear Simulink model
+clear all; clc
+%% Nonlinear identification of parameters of the pendulum model
+% First an approximation of the parameters of the first link is made.
+% Secondly the found parameters are used to find the parameters of the
+% second link
 
-clear all; clc;
+%% Generate a crested multisine input to excite the system with
+addpath('Basic plant')
+calib
+hwinit;
+T = 10;     % duration of expirement
+h = 0.01;   % sample time in seconds
+t = h*(0:T/h)';
+Multisine_generation;
+U = [t xpc2_long]; 
+crinfo
+
+% input for measurement noise
+U = [t zeros(length(t),1)];
+
+return
+% addpath('C:\Users\Dick\Documents\GitHub\control-system-lab\dick\Data\GBN')
+% GBN_input = load('GBN_input1');
+% GBN_output = load('GBN_output1');
+% 
+% time = GBN_input.ans.Time;
+% h = diff(time(1:2));
+% U = GBN_input.ans.Data;
+% y = GBN_output.ans.Data;
+% 
+% fs = 100;                        % sampling frequency
+% f = (1:length(time))./time(end); % Create a frequency vector
+% 
+% U_fft = fft(U);
+% figure(1);
+% subplot(2,1,1);
+% plot(time,U)
+% subplot(2,1,2);
+% loglog(f,abs(U_fft))
+
+
+%%
 b = .5;     % "true" parameter value
 bi = 0.2;   % initial guess
 x0 = [0;0]; % initial state
