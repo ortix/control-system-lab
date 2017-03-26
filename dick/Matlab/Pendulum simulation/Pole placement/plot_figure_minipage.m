57c77d643c53a49f3eceb91a1763a10d0ad1325f
dnieuwenhuizen <dick_n93@hotmail.com>
2017-03-26 11:58:55 +0200

    Alles opgeschoond

--- dick/Matlab/Pendulum simulation/Pole placement/plot_figure_minipage.m
+++
@@ -1,5 +0,0 @@
-set(findall(gcf,'type','axes'),'fontsize',10)
-set(findall(gcf,'type','text'),'fontSize',15) 
-set(gcf, 'PositionMode', 'manual');
-set(gcf, 'Units', 'centimeter');
-set(gcf, 'Position', [10 0 15 12]) % [x y width height]
